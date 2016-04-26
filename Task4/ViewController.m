78296df1cc6cb9c501fafbb05cfd1e695e441f10
young2525 <watanabe.daisuke2525@gmail.com>
2016-04-27 02:04:59 +0900

    ギャラリー呼び出し実装
    [add]機能追加

--- Task4/ViewController.m
+++ Task4/ViewController.m
@@ -1,27 +1,46 @@
 //
 //  ViewController.m
-//  Task4
+//  KD4
 //
-//  Created by watanabe_daisuke on 2016/04/27.
+//  Created by watanabe_daisuke on 2016/04/25.
 //  Copyright © 2016年 watanabe_daisuke. All rights reserved.
 //
 
 #import "ViewController.h"
 
 @interface ViewController ()
-//コメント
+@property (weak, nonatomic) IBOutlet UIButton *ViewController;
 @end
 
 @implementation ViewController
-
+//書き出し
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.imageView = [[UIImageView alloc] init];
+    self.imageView.frame = self.view.frame;
+    [self.view addSubview:self.imageView];
 }
+
+- (void)viewDidAppear:(BOOL)animated {
+    UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
+    imagePicker.delegate = self;
+    [imagePicker setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+    [self presentViewController:imagePicker animated:YES completion:nil];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
 
+
+- (void)imagePickerController :(UIImagePickerController *)picker
+        didFinishPickingImage :(UIImage *)image editingInfo :(NSDictionary *)editingInfo {
+    NSLog(@"selected");
+    [self.imageView setImage:image];
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 @end
