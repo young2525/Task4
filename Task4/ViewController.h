5852da36bc93b03ac71e0c13f212183f45664ceb
young2525 <watanabe.daisuke2525@gmail.com>
2016-04-27 02:03:48 +0900

    イメージピッカー追加
    [add]機能追加

--- Task4/ViewController.h
+++ Task4/ViewController.h
@@ -1,15 +1,14 @@
 //
 //  ViewController.h
-//  Task4
+//  KD4
 //
-//  Created by watanabe_daisuke on 2016/04/27.
+//  Created by watanabe_daisuke on 2016/04/25.
 //  Copyright © 2016年 watanabe_daisuke. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+@interface ViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
-@interface ViewController : UIViewController
-
+@property (strong, nonatomic) UIImageView *imageView;
 
 @end
-
