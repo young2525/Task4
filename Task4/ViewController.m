4bb0c73511b8002375ec885aab344ce93deb8cea
derushio <narumi18wa@gmail.com>
2016-04-28 14:49:19 +0900

    [review] ViewController.m ソースコードレビュー by derushio

--- Task4/ViewController.m
+++ Task4/ViewController.m
@@ -9,16 +9,22 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+// なんでヘッダにUIImageViewを宣言してるのに、UIButtonだけこっちに宣言してるの？
 @property (weak, nonatomic) IBOutlet UIButton *ViewController;
 
+// これも同上の理由でヘッダファイルに書くべき
 - (IBAction)CameraButton:(UIButton *)sender;
 @end
 
 @implementation ViewController
 
+// できればハンドラ系は下の方に書きたいね。
 - (IBAction)CameraButton:(UIButton *)sender {
+    // カメラおしても何もおこらないじゃん。
+    // 何度もループする原因、前に考察したよね？それがしっかり理解できてれば、ループ問題は解決できるはずです。
 }
-//書き出し
+
+//書き出し ← この認識は間違い。この画面のロードが終了した時点で呼ばれるハンドラってだけ。
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
