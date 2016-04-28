10fa2e534dbe63f0b06cae7d95381d659ee623df
young2525 <watanabe.daisuke2525@gmail.com>
2016-04-28 16:23:39 +0900

    [upgrade] ViewController Buttonを押した時にカメラロール呼び出し

--- Task4/ViewController.h
+++ Task4/ViewController.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 @interface ViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>
 
-@property (strong, nonatomic) UIImageView *imageView;
+@property (strong, nonatomic) UIImageView *imageView; //イメージビューを宣言
+@property (weak, nonatomic) IBOutlet UIButton *ViewController; //Buttonを宣言
+
+- (IBAction)CameraButton:(UIButton *)sender;
 
 @end
--- Task4/ViewController.m
+++ Task4/ViewController.m
@@ -9,22 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-// なんでヘッダにUIImageViewを宣言してるのに、UIButtonだけこっちに宣言してるの？
-@property (weak, nonatomic) IBOutlet UIButton *ViewController;
 
-// これも同上の理由でヘッダファイルに書くべき
-- (IBAction)CameraButton:(UIButton *)sender;
 @end
 
 @implementation ViewController
 
-// できればハンドラ系は下の方に書きたいね。
-- (IBAction)CameraButton:(UIButton *)sender {
-    // カメラおしても何もおこらないじゃん。
-    // 何度もループする原因、前に考察したよね？それがしっかり理解できてれば、ループ問題は解決できるはずです。
-}
-
-//書き出し ← この認識は間違い。この画面のロードが終了した時点で呼ばれるハンドラってだけ。
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -33,20 +22,18 @@
     [self.view addSubview:self.imageView];
 }
 
-- (void)viewDidAppear:(BOOL)animated {
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+//ボタンを押した時にカメラロールが表示される
+- (IBAction)CameraButton:(UIButton *)sender {
     UIImagePickerController *imagePicker = [[UIImagePickerController alloc]init];
     imagePicker.delegate = self;
     [imagePicker setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
     [self presentViewController:imagePicker animated:YES completion:nil];
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-    
-}
-
 
 - (void)imagePickerController :(UIImagePickerController *)picker
         didFinishPickingImage :(UIImage *)image editingInfo :(NSDictionary *)editingInfo {
